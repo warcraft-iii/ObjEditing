5a6979b3fcd214aa82f80c0000fb07bd5f290110
Dencer <tdaddon@163.com>
2019-07-17 21:18:00 +0800

    refactor: clean code

--- src/objediting.cpp
+++ src/objediting.cpp
@@ -1,41 +1,10 @@
 #include <cmdline.h>
-
 #include "lua.hpp"
 #include "luaapi.h"
 #include "luasource.h"
 
-constexpr const char* cmdline_map = "map";
-constexpr const char* cmdline_output = "output";
-
-using LuaFile = void (*)(lua_State*, int);
-
-static void InitLuaFiles(lua_State* L, int env)
-{
-    LuaFile files[] = {
-        InitBaseString,
-        InitBaseTable,
-        InitBaseClass,
-        InitCoreChecker,
-        InitCoreEnum,
-        InitCoreObject,
-        InitCoreReadbuffer,
-        InitCoreWritebuffer,
-        InitCoreObjectreader,
-        InitCoreObjectwriter,
-        InitObjectAbilityObjEditing,
-        InitObjectBuffObjEditing,
-        InitObjectDestructableObjEditing,
-        InitObjectItemObjEditing,
-        InitObjectUnitObjEditing,
-        InitObjectUpgradeObjEditing,
-        InitApplication,
-    };
-
-    for (auto file : files)
-    {
-        file(L, env);
-    }
-}
+constexpr const char* CMDLINE_MAP = "map";
+constexpr const char* CMDLINE_OUTPUT = "output";
 
 static void RegisterTo(lua_State* L, lua_CFunction f, const char* table, const char* name)
 {
@@ -48,8 +17,8 @@ static void RegisterTo(lua_State* L, lua_CFunction f, const char* table, const c
 int main(int argc, const char** argv)
 {
     cmdline::parser parser;
-    parser.add<std::string>(cmdline_map, 'm', "The origin map directory");
-    parser.add<std::string>(cmdline_output, 'o', "The output directory");
+    parser.add<std::string>(CMDLINE_MAP, 'm', "The origin map directory");
+    parser.add<std::string>(CMDLINE_OUTPUT, 'o', "The output directory");
 
     if (!parser.parse(argc, argv))
     {
@@ -60,42 +29,76 @@ int main(int argc, const char** argv)
     auto L = luaL_newstate();
     luaL_openlibs(L);
 
-    RegisterTo(L, os_chdir, "os", "chdir");
-
-    lua_newtable(L); // _ENV
+    // init apis
     {
-        lua_newtable(L); // args
-        {
-            for (auto arg : {cmdline_map, cmdline_output})
-            {
-                lua_pushstring(L, parser.get<std::string>(arg).c_str());
-                lua_setfield(L, -2, arg);
-            }
-
-            lua_newtable(L);
-            {
-                auto i = 0;
-                for (auto& file : parser.rest())
-                {
-                    lua_pushstring(L, file.c_str());
-                    lua_rawseti(L, -2, ++i);
-                }
-                lua_setfield(L, -2, "files");
-            }
-
-            lua_setfield(L, -2, "args");
-        }
-
-        lua_newtable(L); // meta
-        {
-            lua_pushglobaltable(L);
-            lua_setfield(L, -2, "__index");
-        }
-
-        lua_setmetatable(L, -2);
+        RegisterTo(L, os_chdir, "os", "chdir");
     }
 
-    InitLuaFiles(L, lua_gettop(L));
+    // init env
+    {
+        lua_newtable(L); // _ENV
+        {
+            lua_newtable(L); // args
+            {
+                for (auto arg : {CMDLINE_MAP, CMDLINE_OUTPUT})
+                {
+                    lua_pushstring(L, parser.get<std::string>(arg).c_str());
+                    lua_setfield(L, -2, arg);
+                }
+
+                lua_newtable(L);
+                {
+                    auto i = 0;
+                    for (auto& file : parser.rest())
+                    {
+                        lua_pushstring(L, file.c_str());
+                        lua_rawseti(L, -2, ++i);
+                    }
+                    lua_setfield(L, -2, "files");
+                }
+
+                lua_setfield(L, -2, "args");
+            }
+
+            lua_newtable(L); // meta
+            {
+                lua_pushglobaltable(L);
+                lua_setfield(L, -2, "__index");
+            }
+            lua_setmetatable(L, -2);
+        }
+    }
+
+    // run files
+    {
+        using LuaFile = void (*)(lua_State*, int);
+
+        LuaFile files[] = {
+            InitBaseString,
+            InitBaseTable,
+            InitBaseClass,
+            InitCoreChecker,
+            InitCoreEnum,
+            InitCoreObject,
+            InitCoreReadbuffer,
+            InitCoreWritebuffer,
+            InitCoreObjectreader,
+            InitCoreObjectwriter,
+            InitObjectAbilityObjEditing,
+            InitObjectBuffObjEditing,
+            InitObjectDestructableObjEditing,
+            InitObjectItemObjEditing,
+            InitObjectUnitObjEditing,
+            InitObjectUpgradeObjEditing,
+            InitApplication,
+        };
+
+        auto env = lua_gettop(L);
+        for (auto file : files)
+        {
+            file(L, env);
+        }
+    }
 
     lua_pop(L, 1);
     lua_close(L);
